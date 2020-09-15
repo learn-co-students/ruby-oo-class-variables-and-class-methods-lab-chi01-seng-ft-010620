5dcc04535268da778793c81d24c53d1523870826
lindsayMecher <lindsaymecher@gmail.com>
2020-09-14 20:06:47 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,56 +1,56 @@
+require "pry"
+
 class Song
+  attr_accessor :name, :artist, :genre 
 
-    @@count = 0
-    @@artists = []
-    @@genres = []
-    @@genre_count = {}
-    @@artist_count = {}
+  @@count = 0 
+  @@artists = []
+  @@genres = []
 
-    attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@count += 1 
+    @@artists << artist 
+    @@genres << genre 
+  end 
 
-    def initialize(name, artist, genre)
-        @name = name
-        @artist = artist
-        @genre = genre
-        @@count += 1
-        @@artists << self.artist
-        @@genres << self.genre
-       
+  def self.count 
+    @@count
+  end 
+
+  def self.artists 
+    @@artists.uniq
+  end
+
+  def self.genres 
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    hash = {}
+    @@genres.each do |genre|
+      if hash[genre]
+        hash[genre] = hash[genre] + 1
+      else  
+        hash[genre] = 1
+      end  
     end
+    hash
+  end 
 
-    def self.count
-        @@count
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      if hash[artist]
+        hash[artist] = hash[artist] + 1
+      else  
+        hash[artist] = 1
+      end  
     end
-    def self.artists
-        @@artists.uniq
-    end
+    hash
+  end 
 
-    def self.genres
-        @@genres.uniq
-    end
 
-    def self.genre_count
-        genre_hash = {}
-        @@genres.map do |genre|
-          if genre_hash[genre]
-            genre_hash[genre] += 1
-          else
-           genre_hash[genre] = 1
-          end
-        end
-      genre_hash
-        
-    end
-
-    def self.artist_count
-      artist_hash = {}
-      @@artists.map do |artist|
-        if artist_hash[artist]
-          artist_hash[artist] += 1
-        else
-          artist_hash[artist] = 1
-        end
-      end
-      artist_hash    
-    end
-end
+end 
